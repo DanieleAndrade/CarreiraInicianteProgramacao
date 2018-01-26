9cd73f37b52ca2b8c13ff9f7d1c36cb3e49bc0e0
Daniele Andrade <danyelebatista@gmail.com>
2018-01-25 23:08:06 -0300

    calculando imc do paciente

--- javascript/introducao-javascript/js/principal.js
+++ javascript/introducao-javascript/js/principal.js
@@ -1,2 +1,13 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent =  "Apareceida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+console.log(imc);
