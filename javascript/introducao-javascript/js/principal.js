feea21167886831f457937af1d95eaae35f89793
Daniele Andrade <danyelebatista@gmail.com>
2018-01-27 23:56:26 -0300

    Pegando valores dos inputs

--- javascript/introducao-javascript/js/principal.js
+++ javascript/introducao-javascript/js/principal.js
@@ -46,6 +46,12 @@ botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
     
     var form = document.querySelector("#form-adiciona");
+    
+    var nome = form.nome.value;
+    var altura = form.altura.value;
+    var peso = form.peso.value;
+    var gordura = form.gordura.value;
+
     console.log(form.altura.value);
     console.log(form.peso.value);
 });
