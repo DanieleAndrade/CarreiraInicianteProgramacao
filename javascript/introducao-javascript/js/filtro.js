e1504e1393de0ed360ba065f2f04e9ce758a833e
Dani <danyelebatista@gmail.com>
2018-02-13 11:41:37 -0300

    Filtrando em uma tabela

---
+++ javascript/introducao-javascript/js/filtro.js
@@ -0,0 +1,16 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+campoFiltro.addEventListener("input", function () {
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.add("invisivel");
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
